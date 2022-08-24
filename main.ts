d07ac8996aee87fd1bfaf2cb55068464745eb64c
Jdomasin <111815565+Jdomasin@users.noreply.github.com>
2022-08-24 12:18:59 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(100)
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(100)
+    basic.showIcon(IconNames.Chessboard)
+    basic.pause(100)
+    basic.showIcon(IconNames.Diamond)
+})
